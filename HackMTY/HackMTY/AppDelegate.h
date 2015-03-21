306cafad22960cc56f1e2ee6eebcdc3fedb72b39
jlrodv <jlrodv@gmail.com>
2015-03-21 11:45:58 -0600

    Project Creation

---
+++ HackMTY/HackMTY/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HackMTY
+//
+//  Created by Jose Luis Rodriguez on 21/03/15.
+//  Copyright (c) 2015 jlrodv. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HackMTY/HackMTY/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HackMTY
+//
+//  Created by Jose Luis Rodriguez on 21/03/15.
+//  Copyright (c) 2015 jlrodv. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
